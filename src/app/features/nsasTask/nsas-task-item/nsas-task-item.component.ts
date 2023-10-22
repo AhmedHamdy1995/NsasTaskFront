908baa0dd45a7c784efc8f26f49446ac081263bd
Ahmed Hamdy <Ahmed.Hamdy2061995@gmail.com>
2023-10-22 23:06:27 +0300

    modifications

--- src/app/features/nsasTask/nsas-task-item/nsas-task-item.component.ts
+++ src/app/features/nsasTask/nsas-task-item/nsas-task-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Statuses } from 'src/app/core/enums/statuses';
 import { NsasTaskDto } from 'src/app/core/models/NsasTask/nsasTaskDto';
 
 @Component({
@@ -6,14 +7,18 @@ import { NsasTaskDto } from 'src/app/core/models/NsasTask/nsasTaskDto';
   templateUrl: './nsas-task-item.component.html',
   styleUrls: ['./nsas-task-item.component.scss']
 })
-export class NsasTaskItemComponent{
+export class NsasTaskItemComponent implements OnInit{
   @Input() task : NsasTaskDto | undefined;
 
   @Output() editTask = new EventEmitter<number>();
   @Output() deleteTask = new EventEmitter<number>();
   @Output() readTask = new EventEmitter<number>();
 
+  status : string ='' ;
   constructor(){}
+  ngOnInit(){
+  this.status = Statuses[this.task!.status]
+  }
 
   onRead(){
     this.readTask.emit(this.task?.id)
